a928c0ae99bb15c3d876572eb370eaac6a3b560a
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-14 17:29:31 +0100

    Added NewTicket view+controller & connected to tickets-btn

---
+++ src/main/java/dk/easv/eventtickets/gui/NewTicketController.java
@@ -0,0 +1,16 @@
+package dk.easv.eventtickets.gui;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class NewTicketController {
+
+    @FXML private MFXTextField txtfieldName;
+    @FXML private MFXTextField txtfieldAmountTickets;
+
+    @FXML
+    private void handlePrintTickets(ActionEvent event) {
+
+    }
+}

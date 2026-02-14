5ea5c6772857db1ae2493fc92b86953a55e712b7
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-14 17:48:35 +0100

    Added NewTicket+PrintTicket views+controller & connected confirm-btn

--- src/main/java/dk/easv/eventtickets/gui/NewTicketController.java
+++ src/main/java/dk/easv/eventtickets/gui/NewTicketController.java
@@ -3,6 +3,10 @@ package dk.easv.eventtickets.gui;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class NewTicketController {
 
@@ -10,7 +14,19 @@ public class NewTicketController {
     @FXML private MFXTextField txtfieldAmountTickets;
 
     @FXML
-    private void handlePrintTickets(ActionEvent event) {
+    private void handleConfirm(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/PrintTicketsView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+
+        } catch (Exception e) {
+            // Show alert-box to user
+            throw new RuntimeException("HandlePrintTickets() failed");
+        }
 
     }
 }
---
+++ src/main/java/dk/easv/eventtickets/gui/PrintTicketController.java
@@ -0,0 +1,11 @@
+package dk.easv.eventtickets.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PrintTicketController {
+
+    @FXML
+    private void handlePrint(ActionEvent event) {
+    }
+}

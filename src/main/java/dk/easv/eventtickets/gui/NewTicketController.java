310726d98a030a3070fe4c9598ba4455ab132026
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-14 18:34:55 +0100

    Added components/Ticket + connected to confirm-btn

--- src/main/java/dk/easv/eventtickets/gui/NewTicketController.java
+++ src/main/java/dk/easv/eventtickets/gui/NewTicketController.java
@@ -4,6 +4,7 @@ import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -15,6 +16,11 @@ public class NewTicketController {
 
     @FXML
     private void handleConfirm(ActionEvent event) {
+
+        // Add safety check for data type
+        int ticketsAmount = Integer.parseInt(txtfieldAmountTickets.getText());
+        System.out.println(ticketsAmount);
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/PrintTicketsView.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
@@ -22,6 +28,16 @@ public class NewTicketController {
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.show();
+
+            // for loop for tickets
+            for (int i = 0; i < ticketsAmount; i++) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/components/Ticket.fxml"));
+                Parent ticketRoot = loader.load();
+                PrintTicketController controller = fxmlLoader.getController();
+                controller.getTicketsContainer().getChildren().add(ticketRoot);
+                System.out.println("tickets added: "+i+1);
+            }
+
 
         } catch (Exception e) {
             // Show alert-box to user

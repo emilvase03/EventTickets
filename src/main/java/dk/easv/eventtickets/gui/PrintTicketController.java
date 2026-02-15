487db156f5d938b4d2ca45d3415763ac5868d003
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-15 19:57:30 +0100

    Implemented ticket print functionality

--- src/main/java/dk/easv/eventtickets/gui/PrintTicketController.java
+++ src/main/java/dk/easv/eventtickets/gui/PrintTicketController.java
@@ -1,5 +1,9 @@
 package dk.easv.eventtickets.gui;
 
+// Project imports
+import dk.easv.eventtickets.gui.utils.TicketPrinter;
+
+// Java imports
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
@@ -10,6 +14,7 @@ public class PrintTicketController {
 
     @FXML
     private void handlePrint(ActionEvent event) {
+        TicketPrinter.print(ticketsContainer);
     }
 
     public FlowPane getTicketsContainer() {

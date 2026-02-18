5e8c1d9a2dc375ffb9cc565befd33aadd0810193
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-18 14:25:57 +0100

    Added columns, and testData to MFXtableView

--- src/main/java/dk/easv/eventtickets/BE/placeholder.java
+++ src/main/java/dk/easv/eventtickets/BE/placeholder.java
@@ -1,4 +1,31 @@
 package dk.easv.eventtickets.BE;
 
 public class placeholder {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+
+    public placeholder(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/dk/easv/eventtickets/GUI/Controllers/AdminDashboardController.java
+++ src/main/java/dk/easv/eventtickets/GUI/Controllers/AdminDashboardController.java
@@ -1,23 +1,74 @@
 package dk.easv.eventtickets.GUI.Controllers;
 
+// Project imports
+import dk.easv.eventtickets.BE.placeholder;
+import dk.easv.eventtickets.GUI.Utils.AlertHelper;
+
+// MaterialFX imports
+import io.github.palexdev.materialfx.controls.MFXPaginatedTableView;
+import io.github.palexdev.materialfx.controls.MFXTableColumn;
+
+// Java imports
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AdminDashboardController {
-    @FXML
-    private VBox allCoordBox;
+public class AdminDashboardController implements Initializable {
+
+    @FXML private MFXPaginatedTableView coordContainer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        MFXTableColumn<placeholder> colName = new MFXTableColumn<>("Name");
+        MFXTableColumn<placeholder> colEmail = new MFXTableColumn<>("Email");
+        MFXTableColumn<placeholder> colUsername = new MFXTableColumn<>("Username");
+
+        colName.setRowCellFactory(name -> new MFXTableRowCell<>(placeholder::getName));
+        colEmail.setRowCellFactory(email -> new MFXTableRowCell<>(placeholder::getEmail));
+        colUsername.setRowCellFactory(username -> new MFXTableRowCell<>(placeholder::getUsername));
+
+        colName.prefWidthProperty().bind(coordContainer.widthProperty().divide(3));
+        colEmail.prefWidthProperty().bind(coordContainer.widthProperty().divide(3));
+        colUsername.prefWidthProperty().bind(coordContainer.widthProperty().divide(3));
+        coordContainer.getTableColumns().addAll(colName, colEmail, colUsername);
+
+        // If TableView needs to be dynamic, in height (max height)
+        //coordContainer.setMaxHeight(Double.MAX_VALUE);
+
+
+        coordContainer.setItems(testData());
+
+    }
+
+    private ObservableList<placeholder> testData() {
+        ObservableList<placeholder> test = FXCollections.observableArrayList();
+        test.add(new placeholder("Jakob", "Jakob@Email.com", "UserJakob", "Jakob"));
+        test.add(new placeholder("Zhovzan", "Zhovzan@Email.com", "UserZhovzan", "Zhovzan"));
+        test.add(new placeholder("Emil", "Emil@Email.com", "UserEmil", "Emil"));
+        test.add(new placeholder("Jakob", "Jakob@Email.com", "UserJakob", "Jakob"));
+        test.add(new placeholder("Zhovzan", "Zhovzan@Email.com", "UserZhovzan", "Zhovzan"));
+        test.add(new placeholder("Emil", "Emil@Email.com", "UserEmil", "Emil"));
+        test.add(new placeholder("Jakob", "Jakob@Email.com", "UserJakob", "Jakob"));
+        test.add(new placeholder("Zhovzan", "Zhovzan@Email.com", "UserZhovzan", "Zhovzan"));
+        test.add(new placeholder("Emil", "Emil@Email.com", "UserEmil", "Emil"));
+
+        return test;
+    }
 
     @FXML
     private void onBtnAddCoord(ActionEvent actionEvent) throws IOException {
-
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/NewCoordinatorView.fxml"));
             Scene scene = new Scene(loader.load());
@@ -27,7 +78,7 @@ public class AdminDashboardController {
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setResizable(false);
             stage.showAndWait();
-            addTheCard();
+            addCard();
 
 
         } catch (IOException e) {
@@ -35,20 +86,18 @@ public class AdminDashboardController {
 
         }
     }
-    public void addTheCard(){
 
+    private void addCard(){
         try{
-        FXMLLoader cardLoader = new FXMLLoader(getClass().getResource("/components/EventCoordCard.fxml"));
-        Parent cardRoot = cardLoader.load();
+            FXMLLoader cardLoader = new FXMLLoader(getClass().getResource("/components/EventCoordCard.fxml"));
+            Parent cardRoot = cardLoader.load();
 
-        // empty
+            // empty
 
-        allCoordBox.getChildren().add(cardRoot);
-    } catch (IOException e)
+            coordContainer.getItems().add(cardRoot);
 
-    {
-        e.printStackTrace();
-
-    }
+        } catch (IOException e) {
+            AlertHelper.showError("Error", "Failed to add Coordinator.");
+        }
     }
 }

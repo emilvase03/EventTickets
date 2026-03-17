50db7f86c7245a556ca618e366e662319c60e9b3
Jakob Primdal <jakobprimdal@gmail.com>
2026-03-17 15:42:09 +0100

    CoordDashboardView: 'Total Events' now updates corresponding to events in EventPool

--- src/main/java/dk/easv/eventtickets/GUI/Controllers/CoordDashboardController.java
+++ src/main/java/dk/easv/eventtickets/GUI/Controllers/CoordDashboardController.java
@@ -5,16 +5,18 @@ import dk.easv.eventtickets.BE.Event;
 import dk.easv.eventtickets.BE.User;
 import dk.easv.eventtickets.BLL.UTIL.UserSession;
 import dk.easv.eventtickets.GUI.Models.EventModel;
-import dk.easv.eventtickets.GUI.Models.UserModel;
 import dk.easv.eventtickets.GUI.Utils.AlertHelper;
 
 // MaterialFX imports
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import io.github.palexdev.mfxcore.controls.Label;
 
 // Java imports
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -29,24 +31,26 @@ public class CoordDashboardController {
 
     private static CoordDashboardController instance;
     private User currentUser;
-    private UserModel userModel = new UserModel();
     private EventModel eventModel;
+    private FXMLLoader newEventLoader;
 
     @FXML private MFXScrollPane scrollPane;
     @FXML private TilePane eventContainer;
+    @FXML private Label lblNumOfEvents;
 
-    private FXMLLoader newEventLoader;
 
     @FXML
     private void initialize() {
         instance = this;
         currentUser = UserSession.getInstance().getCurrentUser();
+
         try {
             eventModel = new EventModel();
         } catch (Exception e) {
             AlertHelper.showError("Error", "EventModel failed in CoordDashboardController.");
         }
 
+        setupListeners();
         setupEventPool(currentUser);
 
         // Preload once when dashboard opens, not when button is clicked
@@ -60,7 +64,6 @@ public class CoordDashboardController {
 
     @FXML
     private void onCreateEvent(ActionEvent event) {
-
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/NewEventView.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
@@ -108,6 +111,24 @@ public class CoordDashboardController {
             AlertHelper.showError("Error","Failed to get my events from event model.");
             System.out.println(e.getMessage());
         }
+    }
+
+    private void setupListeners() {
+        // Event Pool listener, for total events label
+        eventContainer.getChildren().addListener((ListChangeListener<Node>) change -> {
+            int numOfEvents = Integer.parseInt(lblNumOfEvents.getText());
+
+            while (change.next()) {
+
+                if (change.wasAdded()) {
+                    lblNumOfEvents.setText(String.valueOf(numOfEvents+1));
+                }
+                if (change.wasRemoved()) {
+                    lblNumOfEvents.setText(String.valueOf(numOfEvents-1));
+                }
+
+            }
+        });
     }
 
     public void launchTicketsWindow(int ticketsAmount, boolean specialTicket) {
